76214e7a5916f56c5ec9e5fdfae6a850418c0b46
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2023-10-08 18:56:11 +0530

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Welcome() {
+  return (
+    <div className="text-center mt-5">
+      <h1 className="display-4">Welcome!</h1>
+      <p>Thank you for logging in.</p>
+    </div>
+  );
+}
+
+export default Welcome;
---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Use BrowserRouter from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router> {/* Use BrowserRouter from 'react-router-dom' */}
+      <App />
+    </Router>
+  </React.StrictMode>,
+);
+
+reportWebVitals();
